f1c917ad0da77661581a978580565e18cadfb7bb
Rika Watanabe <rika@MacBook.local>
2026-05-18 18:47:01 +0900

    design: サイドバー色調整

--- components/GalleryPage.tsx
+++ components/GalleryPage.tsx
@@ -215,7 +215,7 @@ export default function GalleryPage({ initialItems }: GalleryPageProps) {
           </div>
 
           {displayItems.length > 0 ? (
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
               {displayItems.map((item) => (
                 <div
                   key={
