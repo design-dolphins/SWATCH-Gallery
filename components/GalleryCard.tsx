b84b21e2d74bb8644da743ce8a06c5d76ef4720a
Rika Watanabe <rika@MacBook.local>
2026-05-18 18:50:31 +0900

    design: サイドバー色調整

--- components/GalleryCard.tsx
+++ components/GalleryCard.tsx
@@ -24,10 +24,10 @@ export default function GalleryCard({ item, onOpen, partsCount }: GalleryCardPro
         onClick={() => onOpen(item)}
         aria-label={`${item.site_name ?? item.title}の詳細を見る`}
       >
-        <div className="relative aspect-[16/10] overflow-hidden bg-ink">
+        <div className="relative aspect-[16/10] overflow-hidden bg-white">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            className="absolute inset-0 h-full w-full object-cover transition duration-500 group-hover:scale-[1.035]"
+            className="absolute inset-0 h-full w-full object-contain transition duration-500 group-hover:scale-[1.02]"
             src={item.image_url ?? "/mockups/northstar.svg"}
             alt={item.site_name ?? "Gallery image"}
           />
